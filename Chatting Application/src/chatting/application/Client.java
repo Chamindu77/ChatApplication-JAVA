6853ee4068537aad9cd8e2a37a3b5b056c9abab2
chamindu77 <chamindunipun99@gmail.com>
2024-04-18 18:37:11 +0530

    update

--- Chatting Application/src/chatting/application/Client.java
+++ Chatting Application/src/chatting/application/Client.java
@@ -1,5 +1,6 @@
 package chatting.application;
 
+import static chatting.application.Server.dout;
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
@@ -9,15 +10,19 @@ import java.text.*;
 import java.net.*;
 import java.io.*;
 
-public class Server implements ActionListener{
+public class Client implements ActionListener{
     
     JTextField text;
-    JPanel a1;
+    static JPanel a1;
     static Box vertical = Box.createVerticalBox();
+    
     static JFrame f = new JFrame();
+
+    
     static DataOutputStream dout;
     
-    Server (){
+    
+    Client (){
         
         f.setLayout(null);
         
@@ -40,7 +45,7 @@ public class Server implements ActionListener{
             }
         });
         
-        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/2.png"));
         Image i5 = i4.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT);
         ImageIcon i6= new ImageIcon(i5);
         JLabel profile = new JLabel(i6);
@@ -68,7 +73,7 @@ public class Server implements ActionListener{
         morevert.setBounds(420,20,10,25);
         p1.add(morevert);
         
-        JLabel name = new JLabel ("Somapala");
+        JLabel name = new JLabel ("Siriyawathi");
         name.setBounds(110,15,100,18);
         name.setForeground(Color.WHITE);
         name.setFont(new Font("SAN SERIF",Font.BOLD,18));
@@ -97,7 +102,7 @@ public class Server implements ActionListener{
         f.add(send);
         
         f.setSize(450,700);
-        f.setLocation(200,50);
+        f.setLocation(800,50);
         f.setUndecorated(true);
         f.getContentPane().setBackground(Color.WHITE);
         send.setFont(new Font("SAN_SERIF", Font.PLAIN,16));
@@ -108,32 +113,31 @@ public class Server implements ActionListener{
     }
     
     public void actionPerformed(ActionEvent ae){
-      try{
-        String out = text.getText();
-        
-        
-        JPanel p2 = formatLabel(out);
-        
-        a1.setLayout(new BorderLayout());
-        
-        JPanel right = new JPanel(new BorderLayout());
-        right.add(p2,BorderLayout.LINE_END);
-        vertical.add(right); 
-        vertical.add(Box.createHorizontalStrut(15));
-        
-        a1.add(vertical,BorderLayout.PAGE_START);
-        
-        
-        dout.writeUTF(out);
-        
-        text.setText("");
-        
-        f.repaint();
-        f.invalidate();
-        f.validate();
-      }catch (Exception e){
+        try{
+            String out = text.getText();
+
+
+            JPanel p2 = formatLabel(out);
+
+            a1.setLayout(new BorderLayout());
+
+            JPanel right = new JPanel(new BorderLayout());
+            right.add(p2,BorderLayout.LINE_END);
+            vertical.add(right); 
+            vertical.add(Box.createHorizontalStrut(15));
+
+            a1.add(vertical,BorderLayout.PAGE_START);
+
+            dout.writeUTF(out);
+
+            text.setText("");
+
+            f.repaint();
+            f.invalidate();
+            f.validate();
+        } catch (Exception e){
           e.printStackTrace();
-      }
+        }
         
     }
     
@@ -164,34 +168,31 @@ public class Server implements ActionListener{
     // ijfb hdfb
     
     public static void main(String[] args){
-        new Server(); 
+        new Client(); 
         
-        
-        try {
-        
-            ServerSocket skt = new ServerSocket(6001);
+        try{
+            Socket s = new Socket("127.0.0.1",6001);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            dout = new DataOutputStream(s.getOutputStream());
+            
             while(true){
-                Socket s = skt.accept();
-                DataInputStream din = new DataInputStream(s.getInputStream());
-                dout = new DataOutputStream(s.getOutputStream());
-                
-                while(true){
-                    String msg = din.readUTF();
-                    JPanel panel = formatLabel(msg);
+                a1.setLayout(new BorderLayout());
+                String msg = din.readUTF();
+                JPanel panel = formatLabel(msg);
                     
-                    JPanel left = new JPanel (new BorderLayout());
-                    left.add(panel, BorderLayout.LINE_START);
-                    vertical.add(left);
-                    f.validate();
-                }                
+                JPanel left = new JPanel (new BorderLayout());
+                left.add(panel, BorderLayout.LINE_START);
+                vertical.add(left);
                 
-            }
-        
-        
+                vertical.add(Box.createVerticalStrut(15));
+                a1.add(vertical,BorderLayout.PAGE_START);
+                f.validate();
+            }  
+            
         } catch (Exception e){
             e.printStackTrace();
-            
         }
-    
+        
     }
+    
 }
